0ef01962049713550335b58e9d6a4f661bac868e
Bennett Roesch <meta@spyrosoft.com>
2016-07-27 17:18:09 -0700

    Modify the contact form script to redirect to the blog upon success.

--- awestruct/js/contact.js
+++ awestruct/js/contact.js
@@ -1,4 +1,4 @@
-$('form.contact').submit( contact_form_submission );
+$( 'form.contact' ).submit( contact_form_submission );
 
 function contact_form_submission( submission_event ) {
 	submission_event.preventDefault();
@@ -14,29 +14,23 @@ function contact_form_submission( submission_event ) {
 		return;
 	}
 	$.post( '/contact-ajax/', contact_sumbission )
-		.done(
-			function() {
-				$( 'form.contact' )[0].reset();
-			}
-		)
-		.success(
-			function( response_data ) {
-				var response_json;
-				try {
-					response_json = JSON.parse( response_data );
-					if ( response_json[ 'success' ] ) {
-						alert( 'Thank you for your message.'  );
-					} else {
-						alert( 'Something went wrong. Please contact Bennett by some other means and tell him.' );
-					}
-				} catch ( error ) { alert( 'Something went wrong. Please contact Bennett by some other means and tell him that there was an invalid JSON response.' ); }
-			}
-		)
-		.fail(
-			function() {
-				alert( 'Connection with the server failed. Please check your internet connection. Otherwise, something is wrong on our end - please try again later.' );
-			}
-		);
+		.success( contact_form_success )
+		.fail( contact_form_failure );
 }
 
-document.getElementsByName('name')[0].focus();
+function contact_form_success( response_data ) {
+	var response_json;
+	try {
+		response_json = JSON.parse( response_data );
+		if ( response_json[ 'success' ] ) {
+			alert( 'Thank you! I\'ll respond to you soon. If you are interested, please check out some blog articles.' );
+			window.location = '/blog.html';
+		} else {
+			alert( 'Something went wrong. Please contact Bennett by some other means and tell him.' );
+		}
+	} catch ( error ) { alert( 'Something went wrong. Please contact Bennett by some other means and tell him that there was an invalid JSON response.' ); }
+}
+
+function contact_form_failure() {
+	alert( 'Connection with the server failed. Please check your internet connection. Otherwise, something is wrong on our end - please try again later.' );
+}
