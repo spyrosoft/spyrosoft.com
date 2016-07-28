f3460fb517ddd59fc9585de3053f66916f80655e
Bennett Roesch <meta@spyrosoft.com>
2016-07-27 17:00:12 -0700

    Clean the contact.js script code quite a bit.

--- awestruct/js/contact.js
+++ awestruct/js/contact.js
@@ -1,30 +1,22 @@
 $('form.contact').submit( contact_form_submission );
 
-function contact_form_submission( submission_event )
-{
+function contact_form_submission( submission_event ) {
 	submission_event.preventDefault();
-	var contact_submission_name = $( 'form.contact [name=name]' ).val();
-	var contact_submission_subject = $( 'form.contact [name=subject]' ).val();
-	var contact_submission_email = $( 'form.contact [name=email]' ).val();
-	var contact_submission_message = $( 'form.contact [name=message]' ).val();
-	if ( contact_submission_message === '' )
-	{
+	var contact_sumbission = {
+		'name' : $( 'form.contact [name=name]' ).val(),
+		'subject' : $( 'form.contact [name=subject]' ).val(),
+		'email' : $( 'form.contact [name=email]' ).val(),
+		'message' : $( 'form.contact [name=message]' ).val()
+	};
+	if ( contact_sumbission[ 'message' ] === '' ) {
 		alert( 'The Message field is required.' );
 		$( 'form.contact [name=message]' ).focus();
 		return;
 	}
-	var contact_sumbission_object = new Object;
-	contact_sumbission_object[ 'name' ] = contact_submission_name;
-	contact_sumbission_object[ 'subject' ] = contact_submission_subject;
-	contact_sumbission_object[ 'email' ] = contact_submission_email;
-	contact_sumbission_object[ 'message' ] = contact_submission_message;
-	$.post( '/contact-ajax/', contact_sumbission_object )
+	$.post( '/contact-ajax/', contact_sumbission )
 		.done(
 			function() {
-				$( 'form.contact [name=name]' ).val( '' );
-				$( 'form.contact [name=subject]' ).val( '' );
-				$( 'form.contact [name=email]' ).val( '' );
-				$( 'form.contact [name=message]' ).val( '' );
+				$( 'form.contact' )[0].reset();
 			}
 		)
 		.success(
@@ -37,7 +29,7 @@ function contact_form_submission( submission_event )
 					} else {
 						alert( 'Something went wrong. Please contact Bennett by some other means and tell him.' );
 					}
-				} catch ( error ) { alert( 'Invalid JSON Response.' ); }
+				} catch ( error ) { alert( 'Something went wrong. Please contact Bennett by some other means and tell him that there was an invalid JSON response.' ); }
 			}
 		)
 		.fail(
