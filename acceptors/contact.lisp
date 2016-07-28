e46937e1039c939a63e27c947cbdd7aff1ca9a98
Bennett Roesch <meta@spyrosoft.com>
2016-07-27 17:01:15 -0700

    Modify the contact acceptor to error when email connection fails.

--- acceptors/contact.lisp
+++ acceptors/contact.lisp
@@ -1,17 +1,13 @@
 (defun send-contact-message (name email message)
-  (when (or name email)
-    (if (and name email)
-        (let ((from))
-          (setq from (format nil "~A <~A>" name email))
-          (progn
-            (when name (setq from (format nil "~A" name)))
-            (when email (setq from (format nil "~A" email)))
-            (setq message
-                  (concatenate 'string message
-                               (format nil "~%~%From: ~A" from))))))
-    (setq message
-          (concatenate 'string message
-                       (format nil "~%~%IP Address: ~A" (real-remote-addr)))))
+  (when name (setq message
+                   (concatenate 'string message
+                                (format nil "~%~%Name: ~A" name))))
+  (when email (setq message
+                    (concatenate 'string message
+                                 (format nil "~%~%Email: ~A" email))))
+  (setq message
+        (concatenate 'string message
+                     (format nil "~%~%IP Address: ~A" (real-remote-addr))))
   (handler-case
       (cl-smtp:send-email *outgoing-email-host*
                       *contact-form-sender*
@@ -25,7 +21,8 @@
                                        *outgoing-email-pass*)
                       :extra-headers '(("User-Agent" . ("Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101
  Thunderbird/38.6.0"))))
-    (error nil "")))
+    (error nil nil)
+    ))
 
 (define-easy-handler (contact-form
                       :uri "/contact-ajax/"
@@ -33,7 +30,7 @@
     (name email message)
   (if message
       (progn
-        (send-contact-message name email message)
-        "{\"success\":true}")
-      "{\"success\":false,\"error\":\"The message field is required.\"}")
-  )
+        (if (send-contact-message name email message)
+            "{\"success\":true}"
+            "{\"success\":false,\"error\":\"The message sending process failed. Please notify Bennett by other means if you can.\"}"))
+      "{\"success\":false,\"error\":\"The message field is required.\"}"))
